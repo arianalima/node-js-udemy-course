f6f22851cf3c584e7ddb5fed0c7d29c124f8a327
Ariana Lima <ariana.lima@passeidireto.int>
2021-09-09 14:23:53 -0300

    input commands using yargs

--- notes/app.js
+++ notes/app.js
@@ -1,6 +1,37 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-console.log(getNotes());
+yargs.command({
+	command: 'add',
+	describe: 'Add a new note',
+	handler: function() {
+		console.log('adding note');
+	}
+});
 
-console.log(chalk.green.bold.inverse("Success!"))
+yargs.command({
+	command: 'remove',
+	describe: 'Remove a note',
+	handler: function() {
+		console.log('removing note');
+	}
+});
+
+yargs.command({
+	command: 'read',
+	describe: 'Read a note',
+	handler: function() {
+		console.log('reading note');
+	}
+});
+
+yargs.command({
+	command: 'list',
+	describe: 'Listing notes',
+	handler: function() {
+		console.log('listing notes');
+	}
+});
+
+console.log(yargs.argv);
