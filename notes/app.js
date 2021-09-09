4016d07961f2e708d3b0f1235b3f2db2d94cd49f
Ariana Lima <ariana.lima@passeidireto.int>
2021-09-09 09:35:53 -0300

    use npm chalk

--- notes/app.js
+++ notes/app.js
@@ -1,6 +1,6 @@
-const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
 console.log(getNotes());
 
-console.log(validator.isURL('ari@ari.com'));
+console.log(chalk.green.bold.inverse("Success!"))
