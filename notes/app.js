dcd7c3ff34ea5b2cb8fdfa9b9841ef299bdef55e
Ariana Lima <ariana.lima@passeidireto.int>
2021-09-09 17:49:16 -0300

    actually saving notes in a json file

--- notes/app.js
+++ notes/app.js
@@ -1,12 +1,24 @@
 const chalk = require('chalk');
 const yargs = require('yargs');
-const getNotes = require('./notes.js');
+const notes = require('./notes.js');
 
 yargs.command({
 	command: 'add',
 	describe: 'Add a new note',
-	handler: function() {
-		console.log('adding note');
+	builder: {
+		title: {
+			describe: 'Note title',
+			demandOption: true,
+			type: 'string'
+		},
+		body: {
+			describe: 'Note text',
+			demandOption: true,
+			type: 'string'
+		}
+	},
+	handler: function(argv) {
+		notes.addNote(argv.title, argv.body);
 	}
 });
 
@@ -34,4 +46,4 @@ yargs.command({
 	}
 });
 
-console.log(yargs.argv);
+yargs.parse();
--- notes/notes.js
+++ notes/notes.js
@@ -1,5 +1,37 @@
+const fs = require('fs');
+
 function getNotes() {
-	return 'Your notes...';
+	const notes = loadNotes();
+	console.log(notes);
 }
 
-module.exports = getNotes;
+const addNote = function(title, body) {
+	const notes = loadNotes();
+	const duplicateNotes = notes.filter((note) => note.title === title);
+	if (duplicateNotes.length === 0) {
+		notes.push({
+			title: title,
+			body: body
+		});
+		saveNotes(notes);
+		console.log('Note saved');
+	} else {
+		console.log('Title already exists');
+	}
+};
+
+const loadNotes = function() {
+	try {
+		const dataJSON = fs.readFileSync('notes.json').toString();
+		return JSON.parse(dataJSON);
+	} catch (e) {
+		return [];
+	}
+};
+
+const saveNotes = function(notes) {
+	const dataJSON = JSON.stringify(notes);
+	fs.writeFileSync('notes.json', dataJSON);
+};
+
+module.exports = { getNotes, addNote };
