e17b53bf218d1278a3bf6571c2d15928591c8219
Ariana Lima <ariana.lima@passeidireto.int>
2021-09-15 16:30:54 -0300

    fetch weather api

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,9 @@
-console.log('js file');
+fetch('http://localhost:3000/weather?address=jhgjgj').then((response) => {
+	response.json().then((data) => {
+		if (data.error) {
+			console.log(data.error);
+		} else {
+			console.log(data.forecast, data.location);
+		}
+	});
+});
