c0cd50175df84c51fe93eea9aea20fc75f43b2d3
Ariana Lima <ariana.lima@passeidireto.int>
2021-09-16 17:38:20 -0300

    create a form and call the api within it

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,9 +1,15 @@
-fetch('http://localhost:3000/weather?address=jhgjgj').then((response) => {
-	response.json().then((data) => {
-		if (data.error) {
-			console.log(data.error);
-		} else {
-			console.log(data.forecast, data.location);
-		}
+const weatherForm = document.querySelector('form');
+const searchElement = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	fetch('http://localhost:3000/weather?address=' + searchElement.value).then((response) => {
+		response.json().then((data) => {
+			if (data.error) {
+				console.log(data.error);
+			} else {
+				console.log(data.forecast, data.location);
+			}
+		});
 	});
 });
