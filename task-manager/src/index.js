3e7eb8721bf947d8bc57d0437e40529841af345b
arianalima <ariana.lima@passeidireto.com>
2021-12-22 20:04:23 -0300

    trying bcrypt

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -13,3 +13,17 @@ app.use(taskRouter);
 app.listen(port, () => {
 	console.log('running');
 });
+
+const bcrypt = require('bcryptjs');
+const myFunction = async () => {
+	const pass = 'senha!';
+	const hashedPass = await bcrypt.hash(pass, 8);
+
+	console.log(pass);
+	console.log(hashedPass);
+
+	const isMatch = await bcrypt.compare(pass, hashedPass);
+	console.log(isMatch);
+};
+
+myFunction();
