7610b5314e9f5e63fdfcab332133b018ea2ab897
arianalima <ariana.lima@passeidireto.com>
2022-03-29 13:37:58 -0300

    using env vars

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -4,12 +4,12 @@ const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
 app.listen(port, () => {
-	console.log('running');
+	console.log(`Server is listening on port ${port}`);
 });
