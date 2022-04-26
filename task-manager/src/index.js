2a9ba53aee4699a110254186db59f5af9501136d
arianalima <ariana.lima@passeidireto.com>
2022-04-26 14:49:51 -0300

    refactoring index.js

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,15 +1,7 @@
-const express = require('express');
-require('./db/mongoose');
-const userRouter = require('./routers/user');
-const taskRouter = require('./routers/task');
-
-const app = express();
+const app = require("./app");
 const port = process.env.PORT;
-
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
+const env = process.env.ENV;
 
 app.listen(port, () => {
-	console.log(`Server is listening on port ${port}`);
+  console.log(`Server is listening on ${env}, port ${port}`);
 });
